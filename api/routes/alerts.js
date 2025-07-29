812a268fe4da2d88feb8c876aa224dabb9dbac5a
Nqabenhle <vboxuser@Xubuntu2.myguest.virtualbox.org>
2025-07-29 20:20:24 +0200

    Fix API so that we connect to the database

--- api/routes/alerts.js
+++ api/routes/alerts.js
@@ -6,4 +6,4 @@ const alertsController = require('../controllers/alerts');
 router.get('/:username/alerts/get/all', alertsController.getAll);
 router.get('/:username/alerts/get/:id', alertsController.get);
 
-module.exports = alertsRouter;
+module.exports = router;
